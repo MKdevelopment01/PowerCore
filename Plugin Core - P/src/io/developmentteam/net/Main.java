574ecee43949d87cba4fd67261fedb66309c784e
PowerGamer2007 <59776674+PowerGamer2007@users.noreply.github.com>
2021-01-13 09:47:25 +0100

    playerInfo (2)

--- Plugin Core - P/src/io/developmentteam/net/Main.java
+++ Plugin Core - P/src/io/developmentteam/net/Main.java
@@ -1,5 +1,6 @@
 package io.developmentteam.net;
 
+import io.developmentteam.net.commands.playerInfo;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -8,6 +9,7 @@ public class Main extends JavaPlugin {
     public void onEnable(){
 
         getLogger().info("Enabling PowerCore By KevTheDev and PowerGamerDev");
+        getCommand("playerinfo").setExecutor(new playerInfo());
     }
     public void  onDisable(){
 
