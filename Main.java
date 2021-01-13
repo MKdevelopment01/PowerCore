dafe2b26604b654f4a94b53d4cd3aa7c05217241
KevTheDev <72455370+gamerddd@users.noreply.github.com>
2021-01-13 09:28:38 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
 
+package io.developmentteam.net;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Enabling PowerCore By KevTheDev and PowerGamerDev");
+    }
+
+}
