58695ff508d19d12d5a7fed6bc537f2d9743f637
PowerGamer2007 <59776674+PowerGamer2007@users.noreply.github.com>
2021-01-13 10:01:48 +0100

    playerinfo (3)
    changed the messages of the command

--- Plugin Core - P/src/io/developmentteam/net/commands/playerInfo.java
+++ Plugin Core - P/src/io/developmentteam/net/commands/playerInfo.java
@@ -13,20 +13,20 @@ public class playerInfo implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (player.hasPermission("helpplugin.fly"))
+            if (player.hasPermission("powerplug.playerinfo"))
                 if (args.length == 1) {
                     Player target = Bukkit.getPlayer(args[0]);
                     player.sendMessage(ChatColor.BLUE + target.getPlayer().getName() + "'s Information");
                     player.sendMessage(ChatColor.GRAY + "_________________________________________________");
-                    player.sendMessage(ChatColor.AQUA + "the coords where the player is: " + target.getLocation());
-                    player.sendMessage(ChatColor.AQUA + "the players Health + Food " + ChatColor.RED + "Health: " + target.getHealth() + ChatColor.GOLD + " Food: " + target.getFoodLevel());
-                    player.sendMessage(ChatColor.AQUA + "the GameMode of the player is: " + ChatColor.BLUE + target.getGameMode());
-                    player.sendMessage(ChatColor.AQUA + "fly mode is: " + ChatColor.BLUE + target.getAllowFlight());
+                    player.sendMessage(ChatColor.AQUA + "The coords of the player are: " + target.getLocation());
+                    player.sendMessage(ChatColor.AQUA + "The players Health + Food is: " + ChatColor.RED + "Health: " + target.getHealth() + ChatColor.GOLD + " Food: " + target.getFoodLevel());
+                    player.sendMessage(ChatColor.AQUA + "The GameMode of the player is: " + ChatColor.BLUE + target.getGameMode());
+                    player.sendMessage(ChatColor.AQUA + "Fly mode is: " + ChatColor.BLUE + target.getAllowFlight());
                     player.sendMessage(ChatColor.GRAY + "_________________________________________________");
                 }
         } else {
             Player player = (Player) sender;
-            player.sendMessage(ChatColor.RED + "jou give no playername in the command");
+            player.sendMessage(ChatColor.RED + "you gave not a playername in the command");
             player.sendMessage(ChatColor.RED + "Usage: /playerinfo [player]");
 
         }
